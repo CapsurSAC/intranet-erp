497db3df7a7c7c4b60a3a2fe9a70f071b2643159
CapsurSAC <redes03.capsur@gmail.com>
2026-01-29 15:56:04 -0500

    version bd

--- app/Models/Venta.php
+++ app/Models/Venta.php
@@ -6,10 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Venta extends Model
 {
+    protected $table = 'ventas';
     protected $fillable = [
         'dni',
         'cliente',
         'email',
+        'asesor',
         'curso',
         'asesor',
         'fecha_venta',
