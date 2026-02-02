3e83a2050cbfc913c9eed6596b714a6e6c9cb729
CapsurSAC <redes03.capsur@gmail.com>
2026-02-02 16:34:45 -0500

    webrouting

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,6 @@ use App\Http\Controllers\ImportacionVentaController;
 
 Route::get('/importaciones/ventas', [ImportacionVentaController::class, 'index']);
 Route::post('/importaciones/ventas', [ImportacionVentaController::class, 'store']);
+use App\Http\Controllers\DashboardController; // <--- Asegúrate de que esta línea esté al inicio
+
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
